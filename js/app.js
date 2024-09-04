af1b4981249a96e1a297c03109792744b32f7004
Anish Shukla <shuklaanish040@gmail.com>
2024-09-04 19:08:06 +0530

    first commit

--- js/app.js
+++ js/app.js
@@ -54,6 +54,12 @@ class CalorieTracker {
     this._render();
   }
 
+  setLimit(calorieLimit) {
+    this._calorieLimit = calorieLimit;
+    this._displayCalorieLimit();
+    this._render();
+  }
+
   _displayCaloriesTotal() {
     const totalCaloriesEl = document.getElementById("calories-total");
     totalCaloriesEl.innerHTML = this._totalCalories;
@@ -209,6 +215,10 @@ class App {
     document
       .getElementById("reset")
       .addEventListener("click", this._reset.bind(this));
+
+    document
+      .getElementById("limit-form")
+      .addEventListener("submit", this._setLimit.bind(this));
   }
 
   _newItem(type, e) {
@@ -275,6 +285,23 @@ class App {
       document.getElementById("filter-meals").value = "";
       document.getElementById("filter-workouts").value = "";
     }
+  }
+
+  _setLimit(e) {
+    e.preventDefault();
+    const limit = document.getElementById("limit");
+
+    if (limit.value === "") {
+      alert("Please add a limit");
+      return;
+    }
+
+    this._tracker.setLimit(+limit.value);
+    limit.value = "";
+
+    const modalEl = document.getElementById("limit-modal");
+    const modal = bootstrap.Modal.getInstance(modalEl);
+    modal.hide();
   }
 }
 
